f956566fa735f01b0fcfcb8be9592b4b08ff673c
Alex Flores <domain@me.com>
2021-08-28 22:54:53 -0400

    WIP

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,7 +1,11 @@
 fn main() {
     windows::build!(
         Windows::Win32::UI::WindowsAndMessaging::MessageBoxA,
+        Windows::Win32::Foundation::HANDLE,
         Windows::Win32::Security::{DuplicateTokenEx, ImpersonateLoggedOnUser, AdjustTokenPrivileges},
-        Windows::Win32::System::Threading::{CreateProcessWithTokenW, OpenProcess, OpenProcessToken, GetCurrentProcess, GetCurrentProcessId, GetCurrentThread},
+        Windows::Win32::System::Threading::{
+            CreateProcessWithTokenW, OpenProcess, OpenProcessToken, 
+            GetCurrentProcess, GetCurrentProcessId, GetCurrentThread
+        },
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+use bindings::Windows::Win32::Security::{
+    DuplicateTokenEx, ImpersonateLoggedOnUser, AdjustTokenPrivileges,
+    TOKEN_READ_CONTROL, TOKEN_IMPERSONATE, TOKEN_DUPLICATE,
+};
+use bindings::Windows::Win32::System::Threading::{
+    CreateProcessWithTokenW, OpenProcess, OpenProcessToken,
+    GetCurrentProcess, GetCurrentProcessId, GetCurrentThread,
+    PROCESS_QUERY_INFORMATION,
+};
 use bindings::Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
-use bindings::Windows::Win32::Security::{DuplicateTokenEx, ImpersonateLoggedOnUser, AdjustTokenPrivileges};
-use bindings::Windows::Win32::System::Threading::{CreateProcessWithTokenW, OpenProcess, OpenProcessToken, GetCurrentProcess, GetCurrentProcessId, GetCurrentThread};
+use bindings::Windows::Win32::Foundation::HANDLE;
 
 fn main() {
     unsafe {
-        MessageBoxA(None, "Hello", "World", MB_OK);
+        MessageBoxA(None, "BEFORE", "World", MB_OK);
+        let tokenH = get_token_from_pid(1156);
+        MessageBoxA(None, "MIDDLE", "World", MB_OK);
+        ImpersonateLoggedOnUser(tokenH);
+        MessageBoxA(None, "AFTER", "World", MB_OK);
+    }
+}
+
+fn get_token_from_pid(pid: u32) -> HANDLE {
+     unsafe {
+     let hProc =   OpenProcess(PROCESS_QUERY_INFORMATION, false, pid);
+
+     let newHToken: HANDLE = HANDLE(0);
+
+    let ok = OpenProcessToken(
+        hProc,
+        TOKEN_READ_CONTROL | TOKEN_IMPERSONATE | TOKEN_DUPLICATE ,
+        &newHToken as *const HANDLE as _, 
+        ).as_bool();
+
+    return newHToken;
     }
 }
