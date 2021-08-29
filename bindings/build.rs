127a4c652c68ac3c0a8da5546b70477683295b7d
Alex Flores <domain@me.com>
2021-08-28 20:31:29 -0400

    generate needed function for token manipulation

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     windows::build!(
-        Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK}
+        Windows::Win32::UI::WindowsAndMessaging::MessageBoxA,
+        Windows::Win32::Security::{DuplicateTokenEx, ImpersonateLoggedOnUser, AdjustTokenPrivileges},
+        Windows::Win32::System::Threading::{CreateProcessWithTokenW, OpenProcess, OpenProcessToken, GetCurrentProcess, GetCurrentProcessId, GetCurrentThread},
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use bindings::Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+use bindings::Windows::Win32::Security::{DuplicateTokenEx, ImpersonateLoggedOnUser, AdjustTokenPrivileges};
+use bindings::Windows::Win32::System::Threading::{CreateProcessWithTokenW, OpenProcess, OpenProcessToken, GetCurrentProcess, GetCurrentProcessId, GetCurrentThread};
 
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        MessageBoxA(None, "Hello", "World", MB_OK);
+    }
 }
